40cbbb63bb50ad340e62c2bc5c2d2003c87c4d82
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-08-06 10:40:36 +0300

    delete files

--- js/hw1-5.js
+++ js/hw1-5.js
@@ -31,28 +31,45 @@
 //   return message;
 // }
 
-function getDiscount(totalSpent) {
-  const BASE_DISCOUNT = 0;
-  const BRONZE_DISCOUNT = 0.02;
-  const SILVER_DISCOUNT = 0.05;
-  const GOLD_DISCOUNT = 0.1;
-  let discount;
-  // Change code below this line
-  if (totalSpent => 50000) {
-    discount = GOLD_DISCOUNT;
-  }
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+//   // Change code below this line
+//   if (totalSpent => 50000) {
+//     discount = GOLD_DISCOUNT;
+//   }
 
-  else if (totalSpent => 20000 && totalSpent < 50000){
-    discount = SILVER_DISCOUNT;
-  }
+//   else if (totalSpent => 20000 && totalSpent < 50000){
+//     discount = SILVER_DISCOUNT;
+//   }
   
-  else if (totalSpent => 5000 && totalSpent < 20000){
-discount = BRONZE_DISCOUNT;
-  }
+//   else if (totalSpent => 5000 && totalSpent < 20000){
+// discount = BRONZE_DISCOUNT;
+//   }
 
-    else {
-      discount = BASE_DISCOUNT;
-    }
-  // Change code above this line
-  return discount;
+//     else {
+//       discount = BASE_DISCOUNT;
+//     }
+//   // Change code above this line
+//   return discount;
+// }
+
+function getCommonElements(array1, array2) {
+   // Change code below this line
+   // const array1 = [];
+   //   const array2 = [];
+   let newMas = [];
+   for (let i = 0; i <= array1.length; i++) {
+      if (array2.includes(array1[i])) {
+         newMas.push(array1[i]);
+      }
+
+      return newMas;
+      // Change code above this line
+   }
 }
+
+getCommonElements([1, 2, 3], [2, 1, 17, 19]);
