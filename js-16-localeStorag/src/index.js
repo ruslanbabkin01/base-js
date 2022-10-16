832e4a22ba796ab36f34aedb647837a2bc7a8b2e
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-10-16 10:02:46 +0300

    delete files

--- js-16-localeStorag/src/index.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-import 'material-icons/iconfont/material-icons.css';
--- js-19-lesson/src/js/refs.js
+++
@@ -1,4 +0,0 @@
-export const refs = {
-  weatherWrapper: document.querySelector('.weather__wrapper'),
-  form: document.querySelector('.js-search-form'),
-};
