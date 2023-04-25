e16488a63ed05652334a2651c6b7ee66a18a2b4f
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-04-25 12:42:46 +0300

    add methods copy obj

---
+++ js-05-objects/copyObj.js
@@ -0,0 +1,42 @@
+// Object.assign() використовується для копіювання значень всіх власних властивостей,
+//   що перераховуються, з одного або більше вихідних об'єктів в цільовий об'єкт.Після копіювання він повертає цільовий об'єкт.
+
+// Object.assign(dest, [src1, src2, src3...]);
+// Перший аргумент dest – це цільовий об’єкт, у який ми будемо копіювати.
+// Наступні аргументи src1, ..., srcN (їх може бути скільки завгодно) – це вихідні об’єкти, з яких ми будемо копіювати.
+// Він копіює властивості всіх вихідних об’єктів src1, ..., srcN у цільовийdest.
+// Іншими словами, властивості всіх аргументів, починаючи з другого, копіюються в перший об’єкт.
+// Виклик повертає dest.
+
+const film = {
+  title: 'Matrix',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, nemo iusto! Et doloribus praesentium laboriosam dolorem veritatis, magnam atque amet!',
+  rating: 4,
+  actors: ['Aiden', 'Mason', 'Jimmy'],
+  isAdult: true,
+  location: {
+    country: 'Jamaica',
+    city: 'Ocho Rios',
+    friends: ['Aiden', 'Mason', 'Jimmy'],
+  },
+  salesplan: {
+    mai: 182,
+    june: 50,
+  },
+  getTitle() {
+    return this.title;
+  },
+};
+
+const newFilm = Object.assign({}, film);
+
+const newFilm2 = _.cloneDeep(film);
+
+console.log(newFilm);
+console.log(film.salesplan === newFilm.salesplan); //true
+console.log(film.salesplan === newFilm2.salesplan); //false
+
+const copyFilm = JSON.parse(JSON.stringify(film));
+
+console.log(copyFilm);
